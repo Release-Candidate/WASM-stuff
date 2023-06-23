d1ed847de04610776c6e0247e2f83b7fa6daecf5
Release-Candidate <rec@gmx.at>
2023-06-23 22:02:33 +0200

    Refactor, add main program
    Signed-off-by: Release-Candidate <rec@gmx.at>

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText:  Copyright 2023 Roland Csaszar
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Project:  WASM-stuff
+ * File:     main.ts
+ * Date:     23.Jun.2023
+ *
+ * ==============================================================================
+ */
+
+import { readFileSync } from "node:fs";
+
+/**
+ * Main entry point.
+ */
+export async function main() {
+    const wasmModule = await WebAssembly.compile(
+        readFileSync("./out/test.wasm")
+    );
+    const wasm = (await WebAssembly.instantiate(wasmModule)).exports;
+    // eslint-disable-next-line no-unused-vars
+    const wasmAdd = wasm.add as (a: number, b: number) => number;
+    // eslint-disable-next-line no-console, no-magic-numbers
+    console.log(`WASM add: ${wasmAdd(5, 4)}`);
+}
+
+main();
--- tests/wasm_test.ts
+++ tests/wasm_test.ts
@@ -1,62 +1,61 @@
-// SPDX-FileCopyrightText:  Copyright 2023 Roland Csaszar
-// SPDX-License-Identifier: GPL-3.0-or-later
-//
-// Project:  WASM-stuff
-// File:     wasm_test.ts
-// Date:     23.Jun.2023
-//
-// ==============================================================================
+/*
+ * SPDX-FileCopyrightText:  Copyright 2023 Roland Csaszar
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Project:  WASM-stuff
+ * File:     wasm_test.ts
+ * Date:     23.Jun.2023
+ *
+ * ==============================================================================
+ */
+
+/* eslint-disable no-magic-numbers */
 
 import * as cj from "chai";
 import * as fc from "fast-check";
 import * as nfs from "node:fs";
 
+const wasmPath = "./out/test.wasm";
+
 const int32Prop = fc.integer({ min: -2_147_483_648, max: 2_147_483_647 });
 
+// eslint-disable-next-line no-undef-init
 let wasm: WebAssembly.Exports | undefined = undefined;
 
-before(async () => {
-  const wasmModule = await WebAssembly.compile(
-    nfs.readFileSync("./out/test.wasm")
-  );
-  wasm = (await WebAssembly.instantiate(wasmModule)).exports;
-});
-
 describe("Test test.wat functions", () => {
-  it("WASM export should exist", () => {
-    cj.expect(wasm).not.undefined;
-  });
-  it("Test add", () => {
-    cj.expect((wasm?.add as (x: number, y: number) => number)(1, 2)).to.equal(
-      3
-    );
-  });
-  it("Check add properties: commutative", () => {
-    fc.assert(
-      fc.property(int32Prop, int32Prop, (a, b) => {
-        cj.assert.equal(
-          (wasm?.add as (x: number, y: number) => number)(a, b),
-          (wasm?.add as (x: number, y: number) => number)(b, a)
+    before(async () => {
+        const wasmModule = await WebAssembly.compile(
+            nfs.readFileSync(wasmPath)
         );
-      }),
-      { verbose: true, numRuns: 1000 }
-    );
-  });
-  it("Check add properties: associative", () => {
-    fc.assert(
-      fc.property(int32Prop, int32Prop, int32Prop, (a, b, c) => {
-        cj.assert.equal(
-          (wasm?.add as (x: number, y: number) => number)(
-            (wasm?.add as (x: number, y: number) => number)(a, b),
-            c
-          ),
-          (wasm?.add as (x: number, y: number) => number)(
-            a,
-            (wasm?.add as (x: number, y: number) => number)(b, c)
-          )
+        wasm = (await WebAssembly.instantiate(wasmModule)).exports;
+    });
+    it("WASM export should exist", () => cj.expect(wasm).not.undefined);
+    it("Test add", () => {
+        // eslint-disable-next-line no-unused-vars
+        const wasmAdd = wasm?.add as (x: number, y: number) => number;
+        cj.expect(wasmAdd(1, 2)).to.equal(3);
+    });
+    it("Check add properties: commutative", () => {
+        fc.assert(
+            fc.property(int32Prop, int32Prop, (a, b) => {
+                // eslint-disable-next-line no-unused-vars
+                const wasmAdd = wasm?.add as (x: number, y: number) => number;
+                cj.assert.equal(wasmAdd(a, b), wasmAdd(b, a));
+            }),
+            { verbose: true, numRuns: 1000 }
         );
-      }),
-      { verbose: true, numRuns: 1000 }
-    );
-  });
+    });
+    it("Check add properties: associative", () => {
+        fc.assert(
+            fc.property(int32Prop, int32Prop, int32Prop, (a, b, c) => {
+                // eslint-disable-next-line no-unused-vars
+                const wasmAdd = wasm?.add as (x: number, y: number) => number;
+                cj.assert.equal(
+                    wasmAdd(wasmAdd(a, b), c),
+                    wasmAdd(a, wasmAdd(b, c))
+                );
+            }),
+            { verbose: true, numRuns: 1000 }
+        );
+    });
 });
