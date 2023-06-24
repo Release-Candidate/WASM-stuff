bfe4232c6fad12c1db393340eadf8a327b7a0ecb
Release-Candidate <rec@gmx.at>
2023-06-24 19:51:22 +0200

    Add wasm feature check and wasmedge
    Signed-off-by: Release-Candidate <rec@gmx.at>

--- src/main.ts
+++ src/main.ts
@@ -9,24 +9,47 @@
  * ==============================================================================
  */
 
+/* eslint-disable no-console */
+
 import * as fs from "node:fs";
+import * as proc from "node:process";
+import * as wasc from "wasm-check";
 
 /**
  * Main entry point.
  */
 export async function main() {
+    checkWasmFeatures();
     const wasmModule = await WebAssembly.compile(
         fs.readFileSync("./out/test.wasm")
     );
     const wasm = (await WebAssembly.instantiate(wasmModule)).exports;
     // eslint-disable-next-line no-unused-vars
     const wasmAdd = wasm.add as (a: number, b: number) => number;
-    // eslint-disable-next-line no-console, no-magic-numbers
+    // eslint-disable-next-line no-magic-numbers
     console.log(`WASM add: ${wasmAdd(5, 4)}`);
     // eslint-disable-next-line no-unused-vars
     const wasmFacTC = wasm.facTC as (a: number) => number;
-    // eslint-disable-next-line no-console, no-magic-numbers
+    // eslint-disable-next-line no-magic-numbers
     console.log(`WASM facTC: ${wasmFacTC(5)}`);
+}
+
+/**
+ * Checks and displays Node's supported and unsupported WASM features.
+ */
+function checkWasmFeatures() {
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    function suppString(supported: boolean): string {
+        return supported ? "supported." : "not supported.";
+    }
+    console.log(`Checking WASM features of Node version ${proc.version}`);
+    console.log(`WASM 1.0 ${suppString(wasc.support(1))}`);
+    // eslint-disable-next-line no-magic-numbers
+    console.log(`WASM 2.0 ${suppString(wasc.support(2))}`);
+    for (const [name, supported] of Object.entries(wasc.feature)) {
+        console.log(`${name} ${suppString(supported)}`);
+    }
+    console.log(`Finished checking WASM features.`);
 }
 
 main();
